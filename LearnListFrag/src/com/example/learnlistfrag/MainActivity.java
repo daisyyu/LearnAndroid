b86677a36985c7b1a974bb80baa003a937adae8e
daisyyu <daisyyu1990@gmail.com>
2015-05-04 21:22:19 -0700

    listFragment

--- LearnListFrag/src/com/example/learnlistfrag/MainActivity.java
+++ LearnListFrag/src/com/example/learnlistfrag/MainActivity.java
@@ -1,36 +1,45 @@
 package com.example.learnlistfrag;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		FragmentManager manager = getFragmentManager();
+		FragmentTransaction ft = manager.beginTransaction();
+		MyListFragment listFragment = new MyListFragment();
+		ft.add(R.id.group,listFragment);
+		// adding to backstack so I can click on back button to go back to previous state
+		ft.addToBackStack("MyFragmentList Screen");
+		//Trasaction commit, just like DB
+		ft.commit();
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- LearnListFrag/src/com/example/learnlistfrag/MyListFragment.java
+++ LearnListFrag/src/com/example/learnlistfrag/MyListFragment.java
@@ -6,20 +6,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class MyListFragment extends ListFragment {
-	static final String TAG ="MyListFragment";
+public class MyListFragment extends ListFragment implements
+		AdapterView.OnItemClickListener {
+	static final String TAG = "MyListFragment";
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		Log.d(TAG,"onCreatView is Called");
+		Log.d(TAG, "onCreatView is Called");
 		return inflater.inflate(R.layout.my_list_fragment, container, false);
 	}
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		Log.d(TAG,"onActivityCreated is Called");
+		Log.d(TAG, "onActivityCreated is Called");
 		super.onActivityCreated(savedInstanceState);
-	}   
+		ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+				R.array.heros, android.R.layout.simple_list_item_1);
+		setListAdapter(adapter);
+		getListView().setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Toast.makeText(getActivity(), "Item "+position, Toast.LENGTH_LONG).show();
+		
+	}
 }
